2939c1b7c48e0f0c31a02667427098a58127f321
TigerDemon <tigerdemon555@gmail.com>
2024-07-02 09:51:54 +0900

    create:login, login_create form

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 def main(request):
     return render(request, "main.html")
@@ -13,3 +16,9 @@ def board_list(request):
 
 def board_create(request):
     return render(request, "board_create.html")
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'login_create.html'
